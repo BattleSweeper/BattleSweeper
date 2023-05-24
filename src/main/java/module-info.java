cf223c30fbb4cc7f25689c82a9323a997d6aee29
mooner1022 <siwol@mooner.dev>
2023-05-24 13:35:58 +0900

    Configure lombok dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module dev.battlesweeper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
 
     opens dev.battlesweeper to javafx.fxml;
     exports dev.battlesweeper;
+    exports dev.battlesweeper.objects;
+    opens dev.battlesweeper.objects to javafx.fxml;
 }
